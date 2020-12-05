c610c630d18c398aef2671508e26b9d15129fdb7
Dai Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2020-12-05 15:51:55 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -44,63 +44,63 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-//   // Magnific popup calls
-//    $('#portfolio445').magnificPopup({
-//      delegate: 'a',
-//      type: 'image',
-//      tLoading: 'Loading image #%curr%...',
-//      mainClass: 'mfp-img-mobile',
-//      gallery: {
-//        enabled: true,
-//        navigateByImgClick: true,
-//        preload: [0, 1]
-//      },
-//      image: {
-//        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-//      }
-//    });
-//    $('#portfolio434').magnificPopup({
-//      delegate: 'a',
-//      type: 'image',
-//      tLoading: 'Loading image #%curr%...',
-//      mainClass: 'mfp-img-mobile',
-//      gallery: {
-//        enabled: true,
-//        navigateByImgClick: true,
-//        preload: [0, 1]
-//      },
-//      image: {
-//        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-//      }
-//    });
-//   $('#portfolio450').magnificPopup({
-//      delegate: 'a',
-//      type: 'image',
-//      tLoading: 'Loading image #%curr%...',
-//      mainClass: 'mfp-img-mobile',
-//      gallery: {
-//        enabled: true,
-//        navigateByImgClick: true,
-//        preload: [0, 1]
-//      },
-//      image: {
-//        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-//      }
-//    });
+  // Magnific popup calls
+//   $('#portfolio445').magnificPopup({
+//     delegate: 'a',
+//     type: 'image',
+//     tLoading: 'Loading image #%curr%...',
+//     mainClass: 'mfp-img-mobile',
+//     gallery: {
+//       enabled: true,
+//       navigateByImgClick: true,
+//       preload: [0, 1]
+//     },
+//     image: {
+//       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+//     }
+//   });
+//   $('#portfolio434').magnificPopup({
+//     delegate: 'a',
+//     type: 'image',
+//     tLoading: 'Loading image #%curr%...',
+//     mainClass: 'mfp-img-mobile',
+//     gallery: {
+//       enabled: true,
+//       navigateByImgClick: true,
+//       preload: [0, 1]
+//     },
+//     image: {
+//       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+//     }
+//   });
+//  $('#portfolio450').magnificPopup({
+//     delegate: 'a',
+//     type: 'image',
+//     tLoading: 'Loading image #%curr%...',
+//     mainClass: 'mfp-img-mobile',
+//     gallery: {
+//       enabled: true,
+//       navigateByImgClick: true,
+//       preload: [0, 1]
+//     },
+//     image: {
+//       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+//     }
+//   });
 
-// //   $('#portfolioArt').magnificPopup({
-// //     delegate: 'a',
-// //     type: 'image',
-// //     tLoading: 'Loading image #%curr%...',
-// //     mainClass: 'mfp-img-mobile',
-// //     gallery: {
-// //       enabled: true,
-// //       navigateByImgClick: true,
-// //       preload: [0, 1]
-// //     },
-// //     image: {
-// //       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-// //     }
-// //   });
+//   $('#portfolioArt').magnificPopup({
+//     delegate: 'a',
+//     type: 'image',
+//     tLoading: 'Loading image #%curr%...',
+//     mainClass: 'mfp-img-mobile',
+//     gallery: {
+//       enabled: true,
+//       navigateByImgClick: true,
+//       preload: [0, 1]
+//     },
+//     image: {
+//       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+//     }
+//   });
 })(jQuery); 
 // End of use strict
